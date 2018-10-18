342faec940da401391c011b3b8113d77167117ec
Lauren Hart <laurenannehart@gmail.com>
2018-10-19 12:47:00 +1300

    have server running again

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,22 +1,14 @@
 const path = require('path')
 
 module.exports = {
-  entry: path.join(__dirname, './index.js'),
+  entry: path.join(__dirname, 'index.js'),
   output: {
     path: path.join(__dirname, '../server/public'),
     filename: 'bundle.js'
   },
-  devServer: {
-    publicPath: path.join(__dirname, '../server/public'),
-    contentBase: path.resolve(__dirname, '../server/public'),
-    watchContentBase: true,
-    compress: true,
-    port: 3000
-  },
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      use: 'raw-loader',
       loader: 'babel-loader',
       exclude: /node_modules/
     }]
