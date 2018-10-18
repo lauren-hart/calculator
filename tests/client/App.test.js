59d467f67394cd1cbc2def7ee56539225c11b29c
Lauren Hart <laurenannehart@gmail.com>
2018-10-19 10:08:00 +1300

    have basic test runner is working test

--- tests/client/App.test.js
+++ tests/client/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Enzyme, {shallow, render, mount} from 'enzyme'
+import Enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import './setup-env'
+// import './setup-env'
 
 import App from '../../client/components/App'
-App.prototype.componentDidMount = () => {}
+// App.prototype.componentDidMount = () => {}
 
 Enzyme.configure({adapter: new Adapter()})
 
@@ -18,14 +18,14 @@ test('<App> root has className of app', () => {
   expect(root.length).toBe(1)
 })
 
-test('page header includes fruit', () => {
-  const wrapper = render(<App />)
-  const h1 = wrapper.find('h1')
-  expect(h1.text()).toMatch(/Fruit/)
-})
+// test('page header includes fruit', () => {
+//   const wrapper = render(<App />)
+//   const h1 = wrapper.find('h1')
+//   expect(h1.text()).toMatch(/Fruit/)
+// })
 
-test('renders an <li> for each fruit', () => {
-  const wrapper = mount(<App />)
-  wrapper.setState({fruits: ['orange', 'persimmons', 'kiwi fruit']})
-  expect(wrapper.find('li').length).toBe(3)
-})
+// test('renders an <li> for each fruit', () => {
+//   const wrapper = mount(<App />)
+//   wrapper.setState({fruits: ['orange', 'persimmons', 'kiwi fruit']})
+//   expect(wrapper.find('li').length).toBe(3)
+// })
